a86d3f752e61893e41ac491a66638de043ef00bd
Tarun Pal <139231055+TarunPalTech@users.noreply.github.com>
2026-03-16 15:28:58 +0000

    working...

--- 04_move_zeroes.c
+++ 04_move_zeroes.c
@@ -1,5 +1,7 @@
 /* 4. Write a program to move zeroes at end. */
 
+/* Method - 1 (Zeros gradually moves to the right)*/
+
 void moveZeroes(int a[], int size){
     if(size <= 0) return;
     
@@ -13,5 +15,21 @@ void moveZeroes(int a[], int size){
     while(j<size){
         a[j]=0;
         j++;
+    }
+}
+
+/* Method - 2 (Single Pass method)*/
+
+void moveZeroes(int a[], int size){
+    if(size <= 0) return;
+    
+    int j=0;
+    for(int i=0;i<size;i++){
+        if(a[i]!=0){
+            int temp = a[i];
+            a[i]=a[j];
+            a[j]=temp;
+            j++;
+        }
     }
 }
