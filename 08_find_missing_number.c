f0a9ffbcb8b71f6aa8f77780cf96de3cd26cbf41
Tarun Pal <139231055+TarunPalTech@users.noreply.github.com>
2026-03-17 10:36:16 +0000

    successfully solved....

--- 08_find_missing_number.c
+++ 08_find_missing_number.c
@@ -22,3 +22,36 @@ The sum method can overflow if numbers are very large.
 That’s why many programmers prefer the XOR solution, which is also O(n) but safer.
 */
 
+// Method - 2 (Two Pass Method)
+
+int findMissingNumber(int a[], int size){
+    if(size<=0) return -1;
+    
+    int xor1 = 0;
+    int xor2 = 0;
+    
+    for(int i=0; i<size; i++){
+        xor1 ^= a[i];
+    }
+    
+    for(int i=1; i<=size+1; i++){
+        xor2 ^= i;
+    }
+    
+    return xor1^xor2;
+}
+
+// Method - 3 (Single Pass Method)
+
+int findMissingNumber(int a[], int size){
+    if(size<=0) return -1;
+    
+    int xor = 0;
+    
+    for(int i=0; i<size; i++){
+        xor ^= a[i];
+        xor ^= (i+1);
+    }
+    
+    return xor ^ (size+1);
+}
