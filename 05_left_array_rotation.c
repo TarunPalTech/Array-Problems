5f5f061f1bcdbc31d0641f580c5847957a788d99
Tarun Pal <139231055+TarunPalTech@users.noreply.github.com>
2026-03-17 11:08:16 +0530

    Update 05_left_array_rotation.c

--- 05_left_array_rotation.c
+++ 05_left_array_rotation.c
@@ -15,9 +15,9 @@ void reverse(int a[], int start, int end){
 void leftArrayRotation(int a[], int size, int k){
     k = k % size; /* k = k % size; because rotating an array more than its length repeats the same rotations.*/
     
+    reverse(a, 0, k - 1);
+    reverse(a, k, size - 1);
     reverse(a, 0, size - 1);
-    reverse(a, 0, size - k - 1);
-    reverse(a, size - k, size - 1);
 }
 
 // There are so many other methods like juggling method, prefix sum method, and temporary array buffer method etc.
